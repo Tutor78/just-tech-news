ad0313e09589aeeaaed2e0adb461ef6f3b8bcbea
Michael Tutor <tutor.michael78@gmail.com>
2021-09-06 21:51:21 -0500

    added authentication for users

--- models/User.js
+++ models/User.js
@@ -1,8 +1,14 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
 
 // create User model
-class User extends Model {}
+class User extends Model {
+    // set up method to run on instance data (per user) to check password
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+    }
+}
 
 // define table columns and configuration
 User.init(
@@ -46,6 +52,19 @@ User.init(
     },
     {
         // TABLE CONFIGURATION OPTIONS GO HERE
+
+        hooks: {
+                // set up beforeCreate lifecycle "hook" functionality
+                async beforeCreate(newUserData) {
+                    newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                    return newUserData;
+                },
+                // set up beforeUpdate lifecycle "hook" functionality
+                async beforeUpdate(updatedUserData) {
+                    updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                    return updatedUserData;
+                }
+        },
 
         // pass in our imported sequelize connection
         sequelize,
